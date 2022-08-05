2c3c03009ece1b2c9cdb73f8594b494ba2a06829
Hannah Stampke <hannah.stampke@gmail.com>
2022-08-05 23:24:37 +0930

    Forgot to add skeleton files to assets, and also added my favourite colours to the colours.js file!

---
+++ src/assets/colours.js
@@ -0,0 +1,9 @@
+export const colours = {
+    feijoa: '#a2d187',
+    rhino: '#2e4763',
+    riverbed: '#404a59',
+    malibu: '#78baf6',
+    jaggedice: '#cae9eb',
+    terracotta: '#e17051',
+    hopbush: '#ce6897',
+}
